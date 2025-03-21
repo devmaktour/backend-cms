0ea167f6282528c2ba024b36b58002f0bd826f1e
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2025-03-21 09:11:06 +0700

    add custom controller for tnc

--- src/api/tnc/controllers/tnc.ts
+++ src/api/tnc/controllers/tnc.ts
@@ -4,4 +4,20 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::tnc.tnc');
+export default factories.createCoreController('api::tnc.tnc', ({ strapi }) => ({
+    async find(ctx) {
+        const tnc = await strapi.db.query('api::tnc.tnc').findMany({
+            where: {
+                published_at: { $notNull: true },
+                locale: ['en', 'id'],
+            }
+        })
+
+        if (!tnc || tnc.length === 0) {
+            return ctx.notFound('TnC not found');
+        }
+
+        const sanitizedEntity = await this.sanitizeOutput(tnc, ctx);
+        return this.transformResponse(sanitizedEntity);
+    },
+}));
--- src/api/tnc/routes/tnc.ts
+++ src/api/tnc/routes/tnc.ts
@@ -4,10 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::tnc.tnc', {
-    config: {
-        find: {
-            middlewares: ['api::tnc.tnc-populate']
-        }
-    }
-});
+export default factories.createCoreRouter('api::tnc.tnc');
